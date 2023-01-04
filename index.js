aa592837ddacbd86f4ee153457f48e52566623f0
anshal1 <101871035+anshal1@users.noreply.github.com>
2023-01-04 19:10:27 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const String = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illum unde nulla, distinctio placeat ad at corrupti cupiditate maxime iure. 1 Anshal is a good boy Hello World. 2 Anshal is a good boy Hello World Nice to meet you"]
+
+const printAsItIs = (string) => {
+    const splitText = string[0].split(" ");
+    console.log(splitText);
+}
+printAsItIs(String);
