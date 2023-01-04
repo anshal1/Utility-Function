be489e9de530439d7ccf67e2e2b818d07da0a6bf
anshal1 <101871035+anshal1@users.noreply.github.com>
2023-01-04 19:37:50 +0530

    Printasitis function is completed

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const String = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illum unde nulla, distinctio placeat ad at corrupti cupiditate maxime iure. 1 Anshal is a good boy Hello World. 2 Anshal is a good boy Hello World Nice to meet you"]
+const String = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illum unde nulla, distinctio placeat ad at corrupti cupiditate maxime iure. 1 Anshal is a good boy Hello World. 2 Anshal is a good boy Hello World Nice to meet you. 3 wrtwurtqwurtqwiurtqwirut 4 qwriyqwirutqwiurtqwruiqwtuirty"
 
 const printAsItIs = (string) => {
-    const splitText = string[0].split(" ");
-    console.log(splitText);
+    const splitText = string.split(" ");
+    let newtext = "";
+    splitText.forEach((text) => {
+        if (Number(parseInt(text))) {
+            newtext += ` </br>${text} `
+        } else {
+            newtext += ` ${text} `;
+        }
+    })
+    const element = document.createElement("p");
+    element.setAttribute("class", "text")
+    element.innerHTML = newtext
+    document.body.appendChild(element);
 }
 printAsItIs(String);
