72ff2f918871857d23d1dfad70431cdc3e53a591
anshal1 <101871035+anshal1@users.noreply.github.com>
2023-01-04 19:45:53 +0530

    Little bit of Style added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const String = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illum unde nulla, distinctio placeat ad at corrupti cupiditate maxime iure. 1 Anshal is a good boy Hello World. 2 Anshal is a good boy Hello World Nice to meet you. 3 wrtwurtqwurtqwiurtqwirut 4 qwriyqwirutqwiurtqwruiqwtuirty"
+const String = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illum unde nulla, distinctio placeat ad at corrupti cupiditate maxime iure Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illum unde nulla, distinctio placeat ad at corrupti cupiditate maxime iure.. 1 Anshal is a good boy Hello World. 2 Anshal is a good boy Hello World Nice to meet you. 3 wrtwurtqwurtqwiurtqwirut 4 qwriyqwirutqwiurtqwruiqwtuirty"
 
 const printAsItIs = (string) => {
     const splitText = string.split(" ");
     let newtext = "";
     splitText.forEach((text) => {
         if (Number(parseInt(text))) {
-            newtext += ` </br>${text} `
+            newtext += ` </br>${text} - `
         } else {
             newtext += ` ${text} `;
         }
